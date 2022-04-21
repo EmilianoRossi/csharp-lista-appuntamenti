e43df98014bf0d08ce0e73bc9db76b1b58853101
Emiliano Rossi <emiliano.rossi658@hotmail.com>
2022-04-21 14:44:22 +0200

    Classe, costruttore, get e metodo stampa dati istanziati.

---
+++ ListaAppuntamenti/Appuntamento.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListaAppuntamenti
+{
+    //classe
+    internal class Appuntamento
+    {
+        //Attributi
+        public DateTime dataAppuntamento = new DateTime();
+        public string nomeAppuntamento;
+        public string localitaAppuntamento;
+
+        //costruttore
+        public Appuntamento(DateTime dataAppuntamento , string nomeAppuntamento , string localitaAppuntamento)
+        {
+
+            this.dataAppuntamento = dataAppuntamento;
+            this.nomeAppuntamento = nomeAppuntamento;   
+            this.localitaAppuntamento = localitaAppuntamento;    
+
+        }
+        //Get e set
+
+        public DateTime GetDataAppuntamento()
+        {
+
+            return dataAppuntamento;
+
+        }
+        public string GetNomeAppuntamento()
+        {
+
+            return nomeAppuntamento;
+
+        }
+
+        public string GetLocalitaAppuntamento()
+        {
+
+            return localitaAppuntamento;
+
+        }
+
+        public virtual void ToString()
+        {
+
+
+            Console.WriteLine("------------- Info Appuntamento ---------------");
+            Console.WriteLine("Data appuntamento: " + dataAppuntamento);
+            Console.WriteLine("Nome appuntamento: " + nomeAppuntamento);
+            Console.WriteLine("Luogo appuntamento: " + localitaAppuntamento);
+            Console.WriteLine("-----------------------------------------------");
+
+        }
+
+
+    }
+}
