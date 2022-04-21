8a839894093f7c2176c1d673d58882daffb5e225
Emiliano Rossi <emiliano.rossi658@hotmail.com>
2022-04-21 14:43:47 +0200

    l'utente inserisce il numero n di eventi da aggiungere, istanzio tanti oggetti appuntamento quanti n ha inserito l'utente

--- ListaAppuntamenti/Program.cs
+++ ListaAppuntamenti/Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using ListaAppuntamenti;
+
+List<Appuntamento> listaAppuntamenti = new List<Appuntamento>();
+
+Console.WriteLine("------------Benvenuto nella tua agenda personale--------------");
+Console.WriteLine("------Inserisci il numero di eventi che vuoi aggiungere-------");
+Console.WriteLine("--------------------------------------------------------------");
+int numeroAppuntamentiDaInserire = int.Parse(Console.ReadLine());
+
+for(int i = 0; i < numeroAppuntamentiDaInserire; i++)
+{
+    int numeroEvento = i + 1;
+    Console.WriteLine("Dati evento numero: " + numeroEvento);
+    Console.WriteLine("Inserisci data appuntamento");
+    string dataAppuntamento = Console.ReadLine();
+    DateTime dataUtente = DateTime.Parse(dataAppuntamento);
+    Console.WriteLine("Inserisci nome appuntamento");
+    string nomeAppuntamento = Console.ReadLine();
+    Console.WriteLine("Inserisci luogo appuntamento");
+    string luogoAppuntamento= Console.ReadLine();
+    Console.Clear();
+    Appuntamento nuovoAppuntamento = new Appuntamento(dataUtente , nomeAppuntamento , luogoAppuntamento);
+    listaAppuntamenti.Add(nuovoAppuntamento);
+}
+
+foreach (Appuntamento elementoListaAppuntamenti in listaAppuntamenti)
+{
+
+    elementoListaAppuntamenti.ToString();
+
+}
+
+
