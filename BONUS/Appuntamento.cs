9be397c80c465d31093edecb2dcc93d9b7032bfd
Emiliano Rossi <emiliano.rossi658@hotmail.com>
2022-04-21 18:36:38 +0200

    Bonus completato

---
+++ BONUS/Appuntamento.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BONUS
+{
+    //classe
+    internal class Appuntamento
+    {
+        //Attributi
+        private protected DateTime dataAppuntamento = new DateTime();
+        private protected string nomeAppuntamento;
+        private protected string localitaAppuntamento;
+        private protected DateTime dataAttuale = DateTime.Now;
+
+        //costruttore
+        public Appuntamento(DateTime dataAppuntamento, string nomeAppuntamento, string localitaAppuntamento)
+        {
+
+            this.dataAppuntamento = dataAppuntamento;
+            this.nomeAppuntamento = nomeAppuntamento;
+            this.localitaAppuntamento = localitaAppuntamento;
+            try
+            {
+                ControllaEccezioni();
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine(ex.Message);
+
+            }
+
+        }
+        //Get e set
+
+        public DateTime GetDataAppuntamento()
+        {
+
+            return dataAppuntamento;
+
+        }
+        public string GetNomeAppuntamento()
+        {
+
+            return nomeAppuntamento;
+
+        }
+
+        public string GetLocalitaAppuntamento()
+        {
+
+            return localitaAppuntamento;
+
+        }
+
+        //override metodo tostring per stampare dati
+        public virtual void ToString()
+        {
+
+
+            Console.WriteLine("------------- Info Appuntamento ---------------");
+            Console.WriteLine("Data appuntamento: " + dataAppuntamento);
+            Console.WriteLine("Nome appuntamento: " + nomeAppuntamento);
+            Console.WriteLine("Luogo appuntamento: " + localitaAppuntamento);
+            Console.WriteLine("-----------------------------------------------");
+
+        }
+
+        public DateTime CambiaData(DateTime dataUtente)
+        {
+
+            if (dataUtente < dataAttuale)
+            {
+
+                throw new InvalidOperationException("La data inserita è nel passato");
+                return this.dataAppuntamento;
+
+            }
+            else if (dataUtente > dataAttuale)
+            {
+
+                this.dataAppuntamento = dataUtente;
+                Console.WriteLine("La data inserita è stata aggiornata correttamente");
+                return this.dataAppuntamento;
+
+            }
+            else
+            {
+
+                Console.WriteLine("La data inserita è quella attuale");
+                return this.dataAppuntamento;
+
+            }
+
+        }
+        //BONUS
+        public string CambiaNome(string nomeUtente)
+        {
+
+            if (nomeUtente == nomeAppuntamento)
+            {
+                Console.WriteLine("Il nome dell'appuntamento è uguale al precedente");
+                return this.nomeAppuntamento;
+
+            }
+            else
+            {
+
+                this.nomeAppuntamento = nomeUtente;
+                Console.WriteLine("Il nome dell'appuntamento inserito è stata aggiornato correttamente");
+                return this.nomeAppuntamento;
+
+            }
+
+        }
+
+        public string CambiaLocalita(string localitàUtente)
+        {
+
+            if (localitàUtente == localitaAppuntamento)
+            {
+                Console.WriteLine("Il luogo dell'appuntamento è uguale al precedente");
+                return this.localitaAppuntamento;
+
+            }
+            else
+            {
+
+                this.localitaAppuntamento = localitàUtente;
+                Console.WriteLine("Il nome dell'appuntamento inserito è stata aggiornato correttamente");
+                return this.localitaAppuntamento;
+
+            }
+
+        }
+
+        public void ControllaEccezioni()
+        {
+
+            if (dataAppuntamento < dataAttuale)
+            {
+
+                throw new Exception("Data inserita è nel passato , errore");
+
+            }
+
+        }
+
+
+    }
+}
---
+++ BONUS/Program.cs
@@ -0,0 +1,125 @@
+﻿using BONUS;
+
+//Creo lista vuota
+List<Appuntamento> listaAppuntamenti = new List<Appuntamento>();
+
+Console.WriteLine("------------Benvenuto nella tua agenda personale--------------");
+Console.WriteLine("------Inserisci il numero di eventi che vuoi aggiungere-------");
+Console.WriteLine("--------------------------------------------------------------");
+int numeroAppuntamentiDaInserire = int.Parse(Console.ReadLine());
+
+//istanzio tanti oggetti quanti n numeri inserisce l'utente
+for (int i = 0; i < numeroAppuntamentiDaInserire; i++)
+{
+    int numeroEvento = i + 1;
+    Console.WriteLine("Dati evento numero: " + numeroEvento);
+
+    //Inserimento dati
+    Console.WriteLine("Inserisci data appuntamento");
+    string dataAppuntamento = Console.ReadLine();
+
+    DateTime dataUtente = DateTime.Parse(dataAppuntamento);
+
+    Console.WriteLine("Inserisci nome appuntamento");
+    string nomeAppuntamento = Console.ReadLine();
+
+    Console.WriteLine("Inserisci luogo appuntamento");
+    string luogoAppuntamento = Console.ReadLine();
+
+    //Pulisco la console
+    Console.Clear();
+
+    //Istanzio oggetto
+    Appuntamento nuovoAppuntamento = new Appuntamento(dataUtente, nomeAppuntamento, luogoAppuntamento);
+    listaAppuntamenti.Add(nuovoAppuntamento);
+}
+
+//ciclo con il foreach la lista 
+foreach (Appuntamento elementoListaAppuntamenti in listaAppuntamenti)
+{
+
+    elementoListaAppuntamenti.ToString();
+
+}
+
+//ciclo per cambiare data appuntamento
+Console.WriteLine("-------------Questa è la lista dei tuoi appuntamenti-------------");
+foreach (Appuntamento elementoAppuntamenti in listaAppuntamenti)
+{
+    int j = 0;
+    Console.WriteLine("Appuntamento n° " + (j + 1) + " : " + elementoAppuntamenti.GetNomeAppuntamento());
+
+}
+Console.WriteLine("Vuoi cambiare la data dell'appuntamento?");
+string risposta = Console.ReadLine();
+
+
+//BONUS
+switch (risposta)
+{
+
+    case "si":
+        Console.WriteLine("Cosa vuoi modificare? nome , luogo o data?");
+        string risposta2 = Console.ReadLine();
+        switch (risposta2)
+        {
+            case "data":
+                Console.WriteLine("Di quale appuntamento vuoi cambiare la data? inserisci il numero dell'appuntamento che vuoi cambiare");
+                int numeroAppuntamentoDaCambiare = int.Parse(Console.ReadLine());
+                Console.WriteLine("Inserisci nuova data");
+                try
+                {
+                    DateTime nuovaData = DateTime.Parse(Console.ReadLine());
+                    for (int i = 0; i < listaAppuntamenti.Count; i++)
+                    {
+
+                        listaAppuntamenti[i].CambiaData(nuovaData);
+                        listaAppuntamenti[i].ToString();
+                    }
+
+                }
+                catch (InvalidOperationException ex)
+                {
+
+                    Console.WriteLine(ex.Message);
+                }
+
+
+                break;
+
+            case "luogo":
+                Console.WriteLine("Di quale appuntamento vuoi cambiare il luogo? inserisci il numero dell'appuntamento che vuoi cambiare");
+                string luogoDaCambiare = Console.ReadLine();
+                Console.WriteLine("Inserisci nuova luogo");
+
+                string nuovoLuogo = Console.ReadLine();
+                for (int i = 0; i < listaAppuntamenti.Count; i++)
+                {
+
+                    listaAppuntamenti[i].CambiaLocalita(nuovoLuogo);
+                    listaAppuntamenti[i].ToString();
+                }
+                break;
+            case "nome":
+                Console.WriteLine("Di quale appuntamento vuoi cambiare il nome? inserisci il numero dell'appuntamento che vuoi cambiare");
+                string nomeDaCambiare = Console.ReadLine();
+                Console.WriteLine("Inserisci nuova nome");
+
+                string nuovoNome = Console.ReadLine();
+                for (int i = 0; i < listaAppuntamenti.Count; i++)
+                {
+
+                    listaAppuntamenti[i].CambiaNome(nuovoNome);
+                    listaAppuntamenti[i].ToString();
+                }
+                break;
+
+        }
+        break;
+
+    case "no":
+        Console.WriteLine("Arrivederci");
+        break;
+
+}
+
